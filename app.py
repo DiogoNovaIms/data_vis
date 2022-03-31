06cec4a852dbed41ba1cb454b9f2a14fbca38716
DiogoNovaIms <m20210633@novaims.unl.pt>
2022-03-31 17:22:57 +0100

    update

--- app.py
+++ app.py
@@ -1,70 +1,24 @@
 import dash
 from dash import dcc
 from dash import html
-import plotly.graph_objects as go
-import pandas as pd
-import numpy as np 
-
-#check
-# Dataset 'Processing'
-
-df_emissions = pd.read_csv('emission_full.csv')
-
-df_emission_0 = df_emissions.loc[df_emissions['year']==2000]
-
-# Building our Graphs (nothing new here)
-
-data_choropleth = dict(type='choropleth',
-                       locations=df_emission_0['country_name'],  #There are three ways to 'merge' your data with the data pre embedded in the map
-                       locationmode='country names',
-                       z=np.log(df_emission_0['CO2_emissions']),
-                       text=df_emission_0['country_name'],
-                       colorscale='inferno',
-                       colorbar=dict(title='CO2 Emissions log scaled')
-                      )
-
-layout_choropleth = dict(geo=dict(scope='world',  #default
-                                  projection=dict(type='orthographic'
-                                                 ),
-                                  #showland=True,   # default = True
-                                  landcolor='black',
-                                  lakecolor='white',
-                                  showocean=True,   # default = False
-                                  oceancolor='azure'
-                                 ),
-                         
-                         title=dict(text='World Choropleth Map',
-                                    x=.5 # Title relative position according to the xaxis, range (0,1)
-                                   )
-                        )
-
-fig = go.Figure(data=data_choropleth, layout=layout_choropleth)
-
-
-
-# The App itself
+from dash.dependencies import Input, Output
 
 app = dash.Dash(__name__)
 
-server = app.server
-
-
-
-
-app.layout = html.Div(children=[
-    html.H1(children='My First DashBoard'),
-
-    html.Div(children='''
-        Example of html Container
-    '''),
-
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    )
+app.layout = html.Div([
+    dcc.Input(id='input',
+              value='initial value',
+              type='text'),
+    html.Div(id='div')
 ])
 
 
+@app.callback(
+    Output(component_id='div', component_property='children'),
+    [Input(component_id='input', component_property='value')]
+)
+def update_output_div(input_value):
+    return 'You\'ve entered "{}"'.format(input_value)
 
 
 if __name__ == '__main__':
