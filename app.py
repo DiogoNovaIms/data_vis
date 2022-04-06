ce4daef8c5dbe5d7f76678cf1e96fce006cde8b2
DiogoNovaIms <m20210633@novaims.unl.pt>
2022-04-06 21:58:57 +0100

    Big update

--- app.py
+++ app.py
@@ -1,8 +1,55 @@
+from turtle import width
+from click import style
 import dash
 from dash import dcc
 from dash import html
 from dash.dependencies import Input, Output
+import plotly.graph_objects as go
+import pandas as pd
+import numpy as np
 
+
+# Datasets
+
+df_countries = pd.read_csv("GlobalLandTemperaturesByCountry_clean.csv")
+
+#Countries list
+countries = np.unique(df_countries['Country'])
+
+#
+avg_temp = []
+for country in countries:
+    avg_temp.append(df_countries.loc[(df_countries['Country'] == country) & 
+    (df_countries['year']==2008)]['AverageTemperature'].mean())
+
+# World map
+data = [ dict(
+        type = 'choropleth',
+        locations = countries,
+        z = avg_temp,
+        locationmode = 'country names',
+        text = countries,
+        #marker = dict(
+            #line = dict(color = 'rgb(0,0,0)', width = 1)),
+            #colorbar = dict(autotick = True, tickprefix = '',
+            #title = '# Average\nTemperature,\n°C'),
+        #The following line is also needed to create Stream
+        #stream = stream_id
+            )
+       ]
+
+layout = dict(
+    #title = 'Average land temperature in countries 2',
+    geo = dict(
+        showframe = False,
+        showocean = True,
+        oceancolor = 'rgb(0,255,255)',
+        type = 'equirectangular',
+        autosize=False,
+    ),
+)
+
+fig = dict(data=data, layout=layout)
 
 #WHATEVER YOU DO, DO NOT DELETE THIS
 app = dash.Dash(__name__)
@@ -10,19 +57,19 @@ server = app.server
 ###################################
 
 app.layout = html.Div([
-    dcc.Input(id='input',
-              value='initial value',
-              type='text'),
-    html.Div(id='div')
+    dcc.Graph(
+        id='example-graph',
+        figure=fig,
+    )
 ])
 
 
-@app.callback(
-    Output(component_id='div', component_property='children'),
-    [Input(component_id='input', component_property='value')]
-)
-def update_output_div(input_value):
-    return 'You\'ve entered "{}"'.format(input_value)
+#@app.callback(
+#    Output(component_id='div', component_property='children'),
+#    [Input(component_id='input', component_property='value')]
+#)
+#def update_output_div(input_value):
+#    return 'You\'ve entered "{}"'.format(input_value)
 
 ####################
 if __name__ == '__main__':
---
+++ test.py
@@ -0,0 +1,43 @@
+import plotly.express as px
+import dash
+from dash import dcc
+from dash import html
+from dash.dependencies import Input, Output
+import plotly.graph_objects as go
+import pandas as pd
+import numpy as np
+import json
+
+#mapbox api token
+mapbox_token = "pk.eyJ1IjoibTIwMjEwNjMzIiwiYSI6ImNsMW5yNGQ0bjB2OHgzY29ibWo5ZXoxbnIifQ.7h2zkrNJdHMVS9UiflpyVQ"
+
+#Load dataset
+df_countries = pd.read_csv("GlobalLandTemperaturesByCountry_clean.csv")
+
+world_path = 'custom.geo.json'
+with open(world_path) as f:
+    geo_world = json.load(f)
+
+test_date = "2008-05-01"
+#Working dataset
+df = df_countries.loc[df_countries['dt']==test_date]
+
+fig = go.Figure(
+    go.Scattermapbox(
+        lat=df['Latitude'],
+        lon=df['Longitude'],
+        mode='markers',
+    )
+)
+
+
+# Specify layout information
+fig.update_layout(
+    mapbox=dict(
+        accesstoken=mapbox_token,
+        center=go.layout.mapbox.Center(lat=45, lon=-73),
+        zoom=1
+    )
+)
+
+fig.show()
