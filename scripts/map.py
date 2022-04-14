b0ea9ebea138c1c77b58df11fdb53503a525b624
DiogoNovaIms <m20210633@novaims.unl.pt>
2022-04-14 08:30:14 +0100

    functional map, bar plot and table, still ugly

--- scripts/map.py
+++ scripts/map.py
@@ -6,89 +6,6 @@ import utilds_data as ud
 from logzero import logger
 from plotly.subplots import make_subplots
 
-def get_map_from_date(df_raw,mapbox_token,geo_json,date="2010-01-01",attribute="AverageTemperature"):
-
-    plot_df = df_raw.loc[df_raw["dt"]==date]
-
-
-    fig_data = go.Choroplethmapbox(
-        geojson=geo_json,
-        locations=plot_df["Country"],
-        z=plot_df[attribute],
-        zmin=-40,
-        zmax=40,
-        customdata=plot_df[attribute],
-        name="",
-        text=plot_df["Country"],
-        hovertemplate="%{text}<br>Average Temperature: %{customdata}°C",
-        colorscale="RdBu",
-        reversescale=True,
-        colorbar=dict(outlinewidth=1,
-            outlinecolor="#333333",
-            len=0.9,
-            lenmode="fraction",
-            orientation="h",
-            xanchor="center",
-            yanchor="bottom",
-            bgcolor=None,
-            y=0.05,
-            #title=dict(text="Cases",
-            #            font=dict(size=14))
-        )
-            #tickvals=[0,1,2,3,4,5,6],
-            #ticktext=["1", "10", "100", "1K", "10K", "100K", "1M"],
-            #tickcolor="#333333",
-            #tickwidth=2,
-            #tickfont=dict(color="#333333",
-            #                size=12)),
-        #reversescale=True
-    )
-
-    fig_layout = go.Layout(
-        mapbox_style="light",
-        mapbox_accesstoken=mapbox_token,
-        #mapbox_center={"lat": 37.0902, "lon": -95.7129},
-        margin={"r":0,"t":0,"l":0,"b":0},
-    )
-
-    
-
-    months = []
-    for k,v in ud.MONTH_LIST.items():
-        button = dict(
-            args=["month",v],
-            label=k,
-            method="restyle"
-        ) 
-        months.append(button)
-                
-
-    fig_layout["updatemenus"] = [dict(
-        type="dropdown",
-        buttons= months,
-        direction="up",
-        showactive=True,
-        xanchor="center",
-        y=0.2,
-        x=0.5,
-        bgcolor = "rgba(0,0,0,0)",
-        bordercolor = "rgba(0,0,0,0)",
-        font = dict(
-            color = "black",
-        ),
-    )]
-
-    #fig_layout["annotations"] = [dict(
-    #    text="Trace type:", showarrow=False,
-    #    x=0, y=1.085, yref="paper", align="left"
-    #)]
-
-    fig=go.Figure(data=fig_data,layout=fig_layout)
-
-    return fig
-
-
-
 def get_yearly_data(df_raw):
 
     df = df_raw.loc[df_raw["year"]>=1900]
@@ -110,6 +27,224 @@ def get_temperature_diff_between(df_by_year,start_date,end_date):
                   
     
     return df
+
+def create_final(df,mapbox_token,geo_json,attribute):
+    fig = go.Figure
+
+    years = np.sort(df["year"].unique())
+    plot_df = df[df["year"]==years[-1]]
+    bar_df = plot_df.nlargest(10,"GHG")
+
+    hottest_temperature = plot_df["AverageTemperature"].max()
+    hottest_data = plot_df.loc[plot_df["AverageTemperature"]==hottest_temperature]
+
+    data = [
+        go.Choroplethmapbox(
+        geojson=geo_json,
+        locations=plot_df["Country"],
+        z=plot_df[attribute],
+        zmin=-40,
+        zmax=40,
+        customdata=plot_df[attribute],
+        text=plot_df["Country"],
+        hovertemplate="%{text}<br>Average Temperature: %{customdata}°C",
+        colorscale="RdBu",
+        reversescale=True,
+        showscale=False,
+        subplot="mapbox",
+        name="choroplethmapbox",
+        colorbar=dict(outlinewidth=1,
+            outlinecolor="#333333",
+            len=0.9,
+            lenmode="fraction",
+            orientation="h",
+            xanchor="center",
+            yanchor="bottom",
+            bgcolor=None,
+            y=0.05,
+            #title=dict(text="Cases",
+            #            font=dict(size=14))
+        )
+    ),
+
+    go.Scattermapbox(
+        lat = plot_df["Latitude"],
+        lon = plot_df["Longitude"],
+        mode = "markers+text",
+        marker=go.scattermapbox.Marker(
+            size= plot_df["GHG"]*300/df["GHG"].max(),
+            sizemin=2,
+            opacity=0.7,
+            color = "rgb(235,0,100)",
+        ),
+        text=plot_df["Country"],
+        subplot="mapbox",
+        name="scattermapbox",
+    ),
+
+    go.Bar(
+        x = bar_df["GHG"],
+        y = bar_df["Country"],
+        orientation="h",
+        xaxis="x",
+        yaxis="y",
+        name="bar",
+        textposition='outside',
+        cliponaxis=False,
+        texttemplate='%{x}<br>%{y}',
+    ),
+
+    go.Table(
+        cells = dict(values=[["Hottest Country",""],[hottest_data["Country"],str(hottest_temperature)+" C°"]],
+        align="center"),
+        name = "table",
+        domain = dict(x=[0,0.19],
+                      y=[0,0.5])
+    )
+    ]
+
+    layout = go.Layout(
+        autosize=True,
+        showlegend=False,
+        hovermode="closest",
+        plot_bgcolor='rgba(0,0,0,0)',
+        margin = dict(
+            l = 0,        # left
+            r = 0,        # right
+            t = 0,        # top
+            b = 0,        # bottom
+        ),
+        mapbox=dict(
+
+            style="light",
+            accesstoken=mapbox_token,
+            bearing=0,
+            pitch=0,
+            domain={
+                'x': [0.2, 1],
+                'y': [0, 1]
+            }
+
+        ),
+
+        xaxis={
+            'domain': [0, 0.15],
+            'range': [0,df["GHG"].max()]
+        },
+
+        yaxis={
+            'domain': [0.6, 1],
+            'tickmode': 'linear',
+            'visible': False,
+            'showticklabels':False,
+        },
+    )
+
+    #Update menus
+    layout["updatemenus"] = [dict(
+        type="buttons",
+        buttons=[dict(
+            label="Play",
+            method="animate",
+            args=[None,
+            dict(frame=dict(duration=250,
+            redraw=True),fromcurrent=True),dict(transition=dict(duration=0,redraw=True),fromCurrent=True)]
+        ),
+        dict(
+            label="Pause",
+            method="animate",
+            args=[[None],
+            dict(frame=dict(duration=0,
+            redraw=True),
+            mode="immediate")]
+        )],
+        direction="left",
+        #pad={"r": 10, "t": 35},
+        showactive=False,
+        x=0.1,
+        xanchor="right",
+        y=0,
+        yanchor="top"
+    )]
+
+    sliders_dict = dict(active=len(years) - 1,
+                        visible=True,
+                        yanchor="top",
+                        xanchor="left",
+                        currentvalue=dict(font=dict(size=20),
+                                        prefix="Date: ",
+                                        visible=True,
+                                        xanchor="right"),
+                        pad=dict(b=10,t=10,l=20),
+                        len=0.875,
+                        x=0,
+                        y=0,
+                        steps=[])
+    
+    fig_frames = []
+    steps = []
+    for year in years:#range(years[0],years[-1],10):
+        plot_df = df[df["year"]==year]
+        bar_df = plot_df.nlargest(10,"GHG")
+        hottest_temperature = plot_df["AverageTemperature"].max()
+        hottest_data = plot_df.loc[plot_df["AverageTemperature"]==hottest_temperature]
+
+        frame = go.Frame(data=[
+            go.Choroplethmapbox(
+                locations=plot_df["Country"],
+                z=plot_df[attribute],
+                customdata=plot_df[attribute],
+                #name="",
+                text=plot_df["Country"],
+                #showscale=False,
+            ),
+            go.Scattermapbox(
+                lat = plot_df["Latitude"],
+                lon = plot_df["Longitude"],
+                mode = "markers+text",
+                marker=go.scattermapbox.Marker(
+                    size= plot_df["GHG"]*300/df["GHG"].max(),
+                    sizemin=2,
+                    opacity=0.7
+                ),
+                text=plot_df["Country"],
+                marker_color = "rgb(235,0,100)",
+            ),
+            go.Bar(
+                x = bar_df["GHG"],
+                y = bar_df["Country"],
+                orientation="h",
+            ),
+            go.Table(
+                cells = dict(values=[["Hottest Country",""],[hottest_data["Country"],str(hottest_temperature)+" C°"]],
+                align="center"),
+            ),
+
+            ],
+            name=str(year),
+            traces = [0, 1, 2, 3])
+
+        fig_frames.append(frame)
+
+        slider_step = dict(
+            args=[[str(year)],
+            dict(mode="immediate",
+            frame=dict(duration=500,
+            redraw=True))],
+            method="animate",
+            label=str(year)
+        )
+
+        steps.append(slider_step)
+
+    sliders_dict["steps"] = steps
+
+    layout.update(sliders=[sliders_dict])
+
+    fig = go.Figure(data=data,layout=layout,frames=fig_frames)
+
+    return fig
+
 
 def create_map_new(df,mapbox_token,geo_json,attribute):
     fig = go.Figure
@@ -160,10 +295,6 @@ def create_map_new(df,mapbox_token,geo_json,attribute):
     ),
     ]
 
-
-    fig_subplots = make_subplots(rows=2,cols=1,shared_yaxes=True,vertical_spacing=0.25)
-
-
     #Layout
     fig_layout = go.Layout(
         mapbox_style="light",
@@ -202,7 +333,7 @@ def create_map_new(df,mapbox_token,geo_json,attribute):
     sliders_dict = dict(active=len(years) - 1,
                         visible=True,
                         yanchor="top",
-                        xanchor="left",
+                        xanchor="center",
                         currentvalue=dict(font=dict(size=20),
                                         prefix="Date: ",
                                         visible=True,
@@ -261,9 +392,6 @@ def create_map_new(df,mapbox_token,geo_json,attribute):
 
     fig = go.Figure(data=fig_data, layout=fig_layout, frames=fig_frames)
 
-    fig_subplots.add_trace(fig_data,row=1,col=1)
-
-
 
     return fig
 
@@ -284,7 +412,7 @@ if __name__ == "__main__":
     #Create the map figure
 
     #df = get_temperature_diff_between(df,1900,2000)
-    fig_map = create_map_new(df_raw,mapbox_token,world,"AverageTemperature")   
+    fig_map = create_final(df_raw,mapbox_token,world,"AverageTemperature")   
     #fig_map = get_map_from_date(df_raw,mapbox_token,world)
 
     save_map = {
--- test.py
+++ test.py
@@ -9,35 +9,92 @@ import numpy as np
 import json
 
 #mapbox api token
-mapbox_token = "pk.eyJ1IjoibTIwMjEwNjMzIiwiYSI6ImNsMW5yNGQ0bjB2OHgzY29ibWo5ZXoxbnIifQ.7h2zkrNJdHMVS9UiflpyVQ"
+mapbox_access_token = "pk.eyJ1IjoibTIwMjEwNjMzIiwiYSI6ImNsMW5yNGQ0bjB2OHgzY29ibWo5ZXoxbnIifQ.7h2zkrNJdHMVS9UiflpyVQ"
 
-#Load dataset
-df_countries = pd.read_csv("GlobalLandTemperaturesByCountry+GHG.csv")
-
-world_path = 'custom.geo.json'
-with open(world_path) as f:
-    geo_world = json.load(f)
-
-test_date = "2008-05-01"
-#Working dataset
-df = df_countries.loc[df_countries['dt']==test_date]
-
-fig = go.Figure(
+data = [
     go.Scattermapbox(
-        lat=df['Latitude'],
-        lon=df['Longitude'],
+        lat=['45.5017'],
+        lon=['-73.5673'],
         mode='markers',
-    )
-)
+        marker=dict(
+            size=14
+        ),
+        name='mapbox 1',
+        text=['Montreal'],
+        subplot='mapbox'
+    ),
+    go.Scattermapbox(
+        lat=['45.7'],
+        lon=['-73.7'],
+        mode='markers',
+        marker=dict(
+            size=14
+        ),
+        name='mapbox 1',
+        text=['Montreal 2'],
+        subplot='mapbox'
+    ),
+    go.Scatter(
+        y=[1, 3, 2],
+        xaxis='x',
+        yaxis='y',
+        name='scatter 1'
+    ),
+    go.Scatter(
+        y=[3, 2, 1],
+        xaxis='x',
+        yaxis='y',
+        name='scatter 2'
+    ),
+    go.Bar(
+        y=[1, 2, 3],
+        xaxis='x2',
+        yaxis='y2',
+        name='scatter 3'
+    ),
+    go.Scatter(
+        y=[3, 2, 1],
+        xaxis='x2',
+        yaxis='y2',
+        name='scatter 4'
+    ),
+]
 
-
-# Specify layout information
-fig.update_layout(
+layout = go.Layout(
+    autosize=True,
+    hovermode='closest',
     mapbox=dict(
-        accesstoken=mapbox_token,
-        center=go.layout.mapbox.Center(lat=45, lon=-73),
-        zoom=1
-    )
+        accesstoken=mapbox_access_token,
+        bearing=0,
+        center=dict(
+            lat=45,
+            lon=-73
+        ),
+        pitch=0,
+        zoom=5,
+        domain={
+            'x': [0.3, 0.6],
+            'y': [0.7, 1.0]
+        }
+    ),
+    xaxis={
+        'domain': [0.3, 0.6]
+    },
+    yaxis={
+        'domain': [0.36, 0.6]
+    },
+    xaxis2={
+        'domain': [0.3, 0.6],
+        'side': 'bottom',
+        'anchor': 'y2'
+    },
+    yaxis2={
+        'domain': [0, 0.3],
+        'anchor': 'x2'
+    },
+    height=700,
+    width=700
 )
 
+fig = go.Figure(data=data, layout=layout)
 fig.show()
