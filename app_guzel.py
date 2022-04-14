e1ab66e99f0de85493f85eb3f29305dc1e0c88d8
Guzel2021 <90806986+Guzel2021@users.noreply.github.com>
2022-04-15 00:43:26 +0100

    Add files via upload

--- app_guzel.py
+++ app_guzel.py
@@ -35,7 +35,7 @@ slider_year = dcc.RangeSlider(
         max=df['year'].max(),
         marks={str(i): '{}'.format(str(i)) for i in
                [1960, 1970, 1980, 1990, 2000, 2013]},
-        value=[1970, 2000],
+        value=[1960, 2013],
         step=1
     )
 
@@ -47,6 +47,9 @@ server = app.server
 
 app.layout = html.Div([
 
+    html.Div([
+            html.H1('Correlation between GHG emission and Increase in average temperature with gdp as element size'),
+        ], id='1st row', style={'width': '50%'}, className='pretty_box'),
     html.Div([
         html.Div([
             dcc.Graph(id='correlation_graph'),
@@ -64,31 +67,26 @@ app.layout = html.Div([
 ######################################################Callbacks#########################################################
 
 @app.callback(
-    [
-        Output("correlation_graph", "figure"),
-    ],
-    [
-        Input("year_slider", "value"),
-        Input("year_slider", "value")
-    ]
-)
-
-def plots(start_date, end_date):
-############################################First Bar Plot##########################################################
-
-    df_0 = df.loc[(df["year"] >= start_date) &
-                        (df["year"] <= end_date)].groupby(["Country", "continent"]).agg(
+    dash.dependencies.Output("correlation_graph", 'figure'),
+    [dash.dependencies.Input('year_slider', 'value')
+    ])
+def update_graph(year):
+    df_0 = df.loc[(df["year"] >= year[0]) &
+                  (df["year"] <= year[1])].groupby(["Country", "continent"]).agg(
         {"gdp": np.sum, "AverageTemperature": np.mean, "GHG": np.sum, "temp_diff": np.sum}).reset_index()
 
+    df_0["gdp_test"] = df_0["gdp"] * 2
+
+############################################First Bar Plot##########################################################
 
 
-    corr_fig = px.scatter(df_0, x="temp_diff", y="GHG", color="continent", template="simple_white", hover_name="Country",
-                             size="gdp_test", size_max=80,
-                             labels={
-                                 "temp_diff": "Increase in average temperature",
-                                 "GHG": "Greenhouse Gases Emissions"
+    corr_fig = px.scatter(df_0, x="temp_diff", y="GHG", size="gdp_test", color="continent",
+           hover_name="Country", log_x=False, size_max=100,
+                  labels={
+                     "temp_diff": "Increase in average temperature between " + str(year[0]) + " and " + str(year[1]) + " years",
+                     "GHG": "Greenhouse Gases Emissions"
+                      }, custom_data=['Country', 'gdp'])
 
-                             },)
 
     corr_layout = dict(title=dict(text='Correlation between average temperature difference and GHG Emissions '),
                                     xaxis=dict(title='Average Temperature'),
@@ -96,6 +94,15 @@ def plots(start_date, end_date):
                                     paper_bgcolor='rgba(0,0,0,0)',
                                     plot_bgcolor='rgba(0,0,0,0)'
                               ))
+
+    corr_fig.update_traces(
+        hovertemplate="<br>".join([
+            "Country: %{customdata[0]}",
+            "GDP: %{customdata[1]}",
+            "Increase in average temperature: %{x}",
+            "Greenhouse Gases Emissions: %{y}",
+        ])
+    )
 
     return go.Figure(data=corr_fig, layout=corr_layout)
 
